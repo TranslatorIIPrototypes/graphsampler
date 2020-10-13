858ca1fda55848986f55eb15d7b224cbf2a16404
Chris Bizon <bizon@rciadmins-mbp.lan>
2020-10-13 12:39:38 -0400

    Removed Test

--- test_counter.py
+++ test_counter.py
@@ -1,16 +1,6 @@
 from neo_interface import neo
 from counter import counter, node_combs
 
-def test_missing():
-    neouri = "bolt://localhost:7687"
-    neouser = "neo4j"
-    neopass = "***REMOVED***"
-    n = neo(neouri, neouser, neopass)
-    ab=['CHEMBL.COMPOUND:CHEMBL2333610', 'CHEBI:86216']
-    nodes, edges = n.get_neighborhood_and_directs(ab, 'chemical_substance', 'chemical_substance', degree=1)
-    c = counter(n)
-    c.count(ab, nodes, edges, 'chemical_substance', 'chemical_substance')
-
 def test_combination():
     n3 = [[1,2,3],[1,2,4]]
     n2 = []
